6bccc9f9138824075a31eb128ae5ba9cb3ceffbf
marcogallen <marco.gallen@gmail.com>
2015-10-24 19:38:11 -0500

    Contact form view fixed

--- resources/views/pages/contacto.blade.php
+++ resources/views/pages/contacto.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <h1>Contacto</h1>
 <p>¿Tienes dudas, quieres saber más sobre Perros perdidos México?</p>
-{!! Form::open(['url'=>'contacto-exito') !!}
+{!! Form::open(['url'=>'contacto-exito']) !!}
 <div class="form-group ">
 	<label>Nombre:</label>
 	<input type="text" name="name" id="name" class="form-control" />
@@ -18,5 +18,5 @@
 	<textarea class="form-control" rows="3" id="comentarios" name="comentarios"> </textarea>
 </div>
 <button type="submit" class="btn btn-default">Enviar Comentarios</button>
-{!! Form::close !!}
+{!! Form::close() !!}
 @stop
